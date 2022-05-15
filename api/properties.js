0136cd8ec7c6820fc3de9e6e5b5f13cdc9908e52
daresamuel <samuel.dare@bimbiphilips.com>
2022-05-15 22:19:25 +0100

    Property Completed

--- api/properties.js
+++ api/properties.js
@@ -5,21 +5,16 @@ const authenticateToken = require('../middleware/authenticateToken')
 const uploadImage = require('./helpers/uploadFile')
 
 
-router.get('/properties', authenticateToken, async (req, res) => {
-    const properties = await Property.query()
-
-    return res.json({
-        statgus: 'success',
-        data: properties
-    })
-})
 
 
-router.post('/properties', authenticateToken, async (req, res) => {
+
+
+// POST /property: Create a property advert
+router.post('/property', authenticateToken, async (req, res) => {
     const user = req.user
 
     // Get property input
-    const {status, type, state, city, address, price, created_on} = req.body
+    const { type, state, city, address, price, created_on} = req.body
     const file = req.files.file
 
     const fileType  = file.mimetype.split('/')[0]
@@ -28,7 +23,6 @@ router.post('/properties', authenticateToken, async (req, res) => {
     const imageUrl =  await uploadImage(filePath, fileType, folder)
 
     const property = await Property.query().insert({
-        status,
         type,
         state,
         city,
@@ -41,10 +35,103 @@ router.post('/properties', authenticateToken, async (req, res) => {
     
 
     return res.json({
-        statgus: 'success',
+        status: 'success',
         data: property
     })
 })
+
+// PATCH /property/<:property-id>: Update property data
+// Note: Include any field you will like to update in your request object and only update those fields
+router.patch('/property/:id', authenticateToken, async (req, res) => {
+    
+    const propertyId = req.params.id
+
+
+    // Get property input
+    const { type, state, city, address, price, created_on} = req.body
+
+    const property = await Property.query().patchAndFetchById(propertyId, {type,state, city, address, price, created_on})
+    
+
+    return res.json({
+        status: 'success',
+        data: property
+    })
+})
+
+// PATCH /property/<:property-id>/sold: Mark a property as sold
+router.patch('/property/:id/sold', authenticateToken, async (req, res) => {
+
+    const propertyId = req.params.id
+
+    // Get property input
+    const { status } = req.body
+
+    const property = await Property.query().patchAndFetchById(propertyId, status)
+    
+
+    return res.json({
+        status: 'success',
+        data: property
+    })
+})
+
+// DELETE /property/<:property-id>: Delete a property advert
+router.delete('/property/:id', authenticateToken, async (req, res) => {
+    const user = req.user
+    const propertyId = req.params.id
+
+    const property =  await Property.query().deleteById(propertyId)
+   
+    
+
+    return res.json({
+        status: 'success',
+        data:  property
+    })
+})
+
+//GET /property/<:property-id>: Get a specific property by ID
+router.get('/property/:id', authenticateToken, async (req, res) => {
+    const user = req.user
+    
+    const propertyId = req.params.id
+
+    const property =  await Property.query().findById(propertyId)
+
+
+    return res.json({
+        status: 'success',
+        data:  property
+    })
+})
+
+
+// GET /property: Get all properties
+router.get('/property', authenticateToken, async (req, res) => {
+    const properties = await Property.query()
+
+    return res.json({
+        status: 'success',
+        data: properties
+    })
+})
+
+//GET /property/search?type=propertyType: Get all properties with a specific type
+router.get('/properties/search', authenticateToken, async (req, res) => {
+
+    // Get property search input
+   const {type} = req.query
+
+   const property = await Property.query().where('type', type)
+
+   return res.json({
+       status: 'success',
+       data: property
+   })
+})
+
+
 
 
 module.exports = router
